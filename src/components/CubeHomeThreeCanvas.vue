6ac4b9306aa87c8fb7879aeab753a2d1f007893d
Sergey Platonov <sergeyplatonov@office.cche>
2022-03-23 20:12:13 +0100

    setup

---
+++ src/components/CubeHomeThreeCanvas.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+<div class="button">Hi</div>
+</template>
+
+<script>
+    export default {
+        name: "CubeHomeThreeCanvas",
+
+        data: () => ({}),
+
+        methods: {},
+    }
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/CubeHome.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div class="home">
+    <CubeHomeThreeCanvas/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import CubeHomeThreeCanvas from "@/components/CubeHomeThreeCanvas.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "CubeHome",
+  components: {
+    CubeHomeThreeCanvas,
+  },
+});
+</script>
